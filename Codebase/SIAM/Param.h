2f0abbaaf47918f8e21a41c18296717d8fb66585
Minki1346 <chlalsrl1346@gmail.com>
2026-04-07 04:47:10 +0000

    SIAM 업로드

---
+++ Codebase/SIAM/Param.h
@@ -0,0 +1,78 @@
+/*******************************************************************************
+* Copyright (c) 2021-2025
+* School of Electrical, Computer and Energy Engineering, Arizona State University
+* Department of Electrical and Computer Engineering, University of Wisconsin-Madison
+* PI: Prof. Yu Cao, Prof. Umit Y. Ogras, Prof. Jae-sun Seo, Prof. Chaitali Chakrabrati
+* All rights reserved.
+*
+* This source code is part of SIAM - a framework to benchmark chiplet-based IMC 
+* architectures with synaptic devices(e.g., SRAM and RRAM).
+* Copyright of the model is maintained by the developers, and the model is distributed under
+* the terms of the Creative Commons Attribution-NonCommercial 4.0 International Public License
+* http://creativecommons.org/licenses/by-nc/4.0/legalcode.
+* The source code is free and you can redistribute and/or modify it
+* by providing that the following conditions are met:
+*
+*  1) Redistributions of source code must retain the above copyright notice,
+*     this list of conditions and the following disclaimer.
+*
+*  2) Redistributions in binary form must reproduce the above copyright notice,
+*     this list of conditions and the following disclaimer in the documentation
+*     and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Developer list:
+*		Gokul Krishnan Email: gkrish19@asu.edu
+*		Sumit K. Mandal Email: skmandal@wisc.edu
+
+* Acknowledgements: Prof.Shimeng Yu and his research group for NeuroSim
+********************************************************************************/
+
+#ifndef PARAM_H_
+#define PARAM_H_
+
+class Param {
+public:
+	Param();
+
+	int operationmode, memcelltype, accesstype, transistortype, deviceroadmap;
+
+	double heightInFeatureSizeSRAM, widthInFeatureSizeSRAM, widthSRAMCellNMOS, widthSRAMCellPMOS, widthAccessCMOS, minSenseVoltage;
+
+	double heightInFeatureSize1T1R, widthInFeatureSize1T1R, heightInFeatureSizeCrossbar, widthInFeatureSizeCrossbar;
+
+	int relaxArrayCellHeight, relaxArrayCellWidth, algoWeightMax, algoWeightMin, pool_stride;
+
+	bool globalBufferType, tileBufferType, peBufferType, chipActivation, reLu, novelMapping, mode, chiplet_structure, fixed_cnt_chiplet;
+
+	double clkFreq, featuresize, readNoise, resistanceOn, resistanceOff, maxConductance, minConductance;
+	int temp, technode, wireWidth, multipleCells;
+	double maxNumLevelLTP, maxNumLevelLTD, readVoltage, readPulseWidth, writeVoltage;
+	double accessVoltage, resistanceAccess;
+	double nonlinearIV, nonlinearity;
+	double writePulseWidth, numWritePulse;
+	double globalBusDelayTolerance, localBusDelayTolerance;
+	double treeFoldedRatio, maxGlobalBusWidth;
+	double sparsity;
+
+	int neuro, multifunctional, parallelWrite, parallelRead;
+	int numlut, numColMuxed, numWriteColMuxed, levelOutput, avgWeightBit, numBitInput, Chiplet_GAccumulator;
+	int numRowSubArray, numColSubArray, row_multiplier, col_multiplier, to_subtract;
+	int cellBit, synapseBit, size_chiplet, cnt_chiplet;
+
+	int XNORparallelMode, XNORsequentialMode, BNNparallelMode, BNNsequentialMode, conventionalParallel, conventionalSequential;
+	int numRowPerSynapse, numColPerSynapse;
+	double AR, Rho, wireLengthRow, wireLengthCol, unitLengthWireResistance, wireResistanceRow, wireResistanceCol;
+};
+
+#endif
---
+++ Codebase/SIAM/formula.h
@@ -0,0 +1,84 @@
+/*******************************************************************************
+* Copyright (c) 2021-2025
+* School of Electrical, Computer and Energy Engineering, Arizona State University
+* Department of Electrical and Computer Engineering, University of Wisconsin-Madison
+* PI: Prof. Yu Cao, Prof. Umit Y. Ogras, Prof. Jae-sun Seo, Prof. Chaitali Chakrabrati
+* All rights reserved.
+*
+* This source code is part of SIAM - a framework to benchmark chiplet-based IMC 
+* architectures with synaptic devices(e.g., SRAM and RRAM).
+* Copyright of the model is maintained by the developers, and the model is distributed under
+* the terms of the Creative Commons Attribution-NonCommercial 4.0 International Public License
+* http://creativecommons.org/licenses/by-nc/4.0/legalcode.
+* The source code is free and you can redistribute and/or modify it
+* by providing that the following conditions are met:
+*
+*  1) Redistributions of source code must retain the above copyright notice,
+*     this list of conditions and the following disclaimer.
+*
+*  2) Redistributions in binary form must reproduce the above copyright notice,
+*     this list of conditions and the following disclaimer in the documentation
+*     and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Developer list:
+*		Gokul Krishnan Email: gkrish19@asu.edu
+*		Sumit K. Mandal Email: skmandal@wisc.edu
+
+* Acknowledgements: Prof.Shimeng Yu and his research group for NeuroSim
+********************************************************************************/
+
+#ifndef NEUROSIM_FORMULA_H_
+#define NEUROSIM_FORMULA_H_
+
+#include "Technology.h"
+
+#define MAX(a,b) (((a)> (b))?(a):(b))
+#define MIN(a,b) (((a)< (b))?(a):(b))
+
+/* Calculate MOSFET gate capacitance */
+double CalculateGateCap(double width, Technology tech);
+
+double CalculateGateArea(
+		int gateType, int numInput,
+		double widthNMOS, double widthPMOS,
+		double heightTransistorRegion, Technology tech,
+		double *height, double *width);
+
+/* Calculate the capacitance of a logic gate */
+void CalculateGateCapacitance(
+		int gateType, int numInput,
+		double widthNMOS, double widthPMOS,
+		double heightTransistorRegion, Technology tech,
+		double *capInput, double *capOutput);
+
+double CalculateDrainCap(
+		double width, int type,
+		double heightTransistorRegion, Technology tech);
+
+double CalculateGateLeakage(
+		int gateType, int numInput,
+		double widthNMOS, double widthPMOS,
+		double temperature, Technology tech);
+
+double CalculateOnResistance(double width, int type, double temperature, Technology tech);
+
+double CalculateTransconductance(double width, int type, Technology tech);
+
+double horowitz(double tr, double beta, double rampInput, double *rampOutput);
+
+double CalculatePassGateArea(double widthNMOS, double widthPMOS, Technology tech, int numFold, double *height, double *width);
+
+double NonlinearResistance(double R, double NL, double Vw, double Vr, double V);
+
+#endif /* FORMULA_H_ */
